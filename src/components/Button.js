47fa89401a25c9fb00d20207ccbf8b1a6ea3512b
Amit Baid <baidstar@hotmail.com>
2021-08-31 18:41:57 -0600

    fix danger button class

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,23 +4,22 @@ import "components/Button.scss";
 
 export default function Button(props) {
    let buttonClass = "button";
-
-   if(props.confirm) {
-      buttonClass += " button--confirm";
-
+ 
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+ 
    if (props.danger) {
-      buttonClass += " button--danger";
-       
-   }   
-}
-
-
-
-   return <button 
-            className={buttonClass}
-            onClick={props.onClick}
-            disabled={props.disabled}
-          >
-            {props.children}
-          </button>;
-}
+     buttonClass += " button--danger";
+   }
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
